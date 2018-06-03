f82a25f9fbf280e98733a9f8eae3d24b779edea0
Mehmet Onay <mhmt.10ay@gmail.com>
2018-06-03 23:23:57 +0300

    First Commit
    First Commit

---
+++ monayspider.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd # dataframe kullanımı için
+import numpy as np #mathematiksel işlemler için
+import matplotlib.pyplot as plt # çizdirme kütüphanesi
+import seaborn as sns # gelişmiş çizdirme kütüphanesi 
+
+df = pd.read_csv('/Users/mehmetonay/Desktop/train.csv')
+
+df.drop(['Customer ID','Network type subscription in Month 1','Network type subscription in Month 2','Most Loved Competitor network in in Month 1','Most Loved Competitor network in in Month 2'],axis =1, inplace=True)# anlamsız sütunları sildim
+newdf = df.iloc[:1399]
+
+X = newdf.drop('Churn Status',axis=1)
+y = newdf['Churn Status']
+
+from sklearn import datasets
+from sklearn import svm
+from sklearn.model_selection import train_test_split # train ve test datalarını ayırmak için kullanılan kütüphane
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30,random_state=44)
+
+from sklearn.preprocessing import StandardScaler # SVM bazında dataların fit olması için vektörel hale getirilmesi gerekir.
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+clf = svm.SVC(kernel='linear')
+
+clf.fit(X_train,y_train)
+
+y_pred = clf.predict(X_test)
+
+from sklearn.metrics import classification_report,confusion_matrix 
+
+cr = classification_report(y_test,y_pred)
+cm = confusion_matrix(y_test,y_pred)
+import matplotlib.pyplot as plt
+import numpy as np
+
+def show_values(pc, fmt="%.2f", **kw):
+    
+    from itertools import izip
+    pc.update_scalarmappable()
+    ax = pc.get_axes()
+    for p, color, value in izip(pc.get_paths(), pc.get_facecolors(), pc.get_array()):
+        x, y = p.vertices[:-2, :].mean(0)
+        if np.all(color[:3] > 0.5):
+            color = (0.0, 0.0, 0.0)
+        else:
+            color = (1.0, 1.0, 1.0)
+        ax.text(x, y, fmt % value, ha="center", va="center", color=color, **kw)
+
+
+def cm2inch(*tupl):
+  
+    inch = 2.54
+    if type(tupl[0]) == tuple:
+        return tuple(i/inch for i in tupl[0])
+    else:
+        return tuple(i/inch for i in tupl)
+
+
+def heatmap(AUC, title, xlabel, ylabel, xticklabels, yticklabels, figure_width=40, figure_height=20, correct_orientation=False, cmap='RdBu'):
+ 
+    # Plot it out
+    fig, ax = plt.subplots()    
+    #c = ax.pcolor(AUC, edgecolors='k', linestyle= 'dashed', linewidths=0.2, cmap='RdBu', vmin=0.0, vmax=1.0)
+    c = ax.pcolor(AUC, edgecolors='k', linestyle= 'dashed', linewidths=0.2, cmap=cmap)
+
+    # put the major ticks at the middle of each cell
+    ax.set_yticks(np.arange(AUC.shape[0]) + 0.5, minor=False)
+    ax.set_xticks(np.arange(AUC.shape[1]) + 0.5, minor=False)
+
+    # set tick labels
+    #ax.set_xticklabels(np.arange(1,AUC.shape[1]+1), minor=False)
+    ax.set_xticklabels(xticklabels, minor=False)
+    ax.set_yticklabels(yticklabels, minor=False)
+
+    # set title and x/y labels
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)      
+
+    # Remove last blank column
+    plt.xlim( (0, AUC.shape[1]) )
+
+    # Turn off all the ticks
+    ax = plt.gca()    
+    for t in ax.xaxis.get_major_ticks():
+        t.tick1On = False
+        t.tick2On = False
+    for t in ax.yaxis.get_major_ticks():
+        t.tick1On = False
+        t.tick2On = False
+
+    # Add color bar
+    plt.colorbar(c)
+
+    # Add text in each cell 
+    show_values(c)
+
+    # Proper orientation (origin at the top left instead of bottom left)
+    if correct_orientation:
+        ax.invert_yaxis()
+        ax.xaxis.tick_top()       
+
+    # resize 
+    fig = plt.gcf()
+    #fig.set_size_inches(cm2inch(40, 20))
+    #fig.set_size_inches(cm2inch(40*4, 20*4))
+    fig.set_size_inches(cm2inch(figure_width, figure_height))
+
+
+
+def plot_classification_report(classification_report, title='Classification report ', cmap='RdBu'):
+    '''
+    Plot scikit-learn classification report.
+    Extension based on https://stackoverflow.com/a/31689645/395857 
+    '''
+    lines = classification_report.split('\n')
+
+    classes = []
+    plotMat = []
+    support = []
+    class_names = []
+    for line in lines[2 : (len(lines) - 2)]:
+        t = line.strip().split()
+        if len(t) < 2: continue
+        classes.append(t[0])
+        v = [float(x) for x in t[1: len(t) - 1]]
+        support.append(int(t[-1]))
+        class_names.append(t[0])
+        print(v)
+        plotMat.append(v)
+
+    print('plotMat: {0}'.format(plotMat))
+    print('support: {0}'.format(support))
+
+    xlabel = 'Metrics'
+    ylabel = 'Classes'
+    xticklabels = ['Precision', 'Recall', 'F1-score']
+    yticklabels = ['{0} ({1})'.format(class_names[idx], sup) for idx, sup  in enumerate(support)]
+    figure_width = 25
+    figure_height = len(class_names) + 7
+    correct_orientation = False
+    heatmap(np.array(plotMat), title, xlabel, ylabel, xticklabels, yticklabels, figure_width, figure_height, correct_orientation, cmap=cmap)
+
+plot_classification_report(cr)
